8082e81ce1a2e1332871a6acf30ec901e51f7248
kassas20 <kassas20@mail.ru>
2024-03-22 02:11:29 +0400

    refactoring autorization and create alembic service in docker

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,14 @@
-from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from app.config import Config
 from flask import Flask
+from settings import settings
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.secret_key = settings.SECRET_KEY
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-jwt = JWTManager(app)
 
 from app.auth.view import router as auth
 from app.bank.view import router as bank
--- app/auth/view.py
+++ app/auth/view.py
@@ -1,6 +1,5 @@
-from flask import jsonify, Blueprint, request
+from flask import Blueprint, request, session
 from flask.views import MethodView
-from flask_jwt_extended import create_access_token
 
 from app.auth.model import User
 from app import db, bcrypt
@@ -10,7 +9,7 @@ class Registration(MethodView):
     @staticmethod
     def post():
         email = request.form['email']
-        user = User.query.filter_by(email=request.form['email']).first()
+        user = db.session.query(User).filter(User.email == request.form['email']).first()
         if user is None:
             user = User(
                 email=email,
@@ -27,10 +26,10 @@ class Login(MethodView):
     def post():
         email = request.form['email']
         password = request.form['password']
-        user = User.query.filter_by(email=request.form['email']).first()
+        user = db.session.query(User).filter_by(email=request.form['email']).first()
         if user is not None and bcrypt.check_password_hash(user.password, password):
-            access_token = create_access_token(identity=email)
-            return jsonify(access_token)
+            session['session'] = email
+            return {'message': 'Entry successful'}
         return {'message': 'Credentials Error'}
 
 
@@ -44,5 +43,5 @@ router.add_url_rule(
 router.add_url_rule(
     '/auth/login',
     view_func=Login.as_view('login_api'),
-    methods=['POST', 'GET']
+    methods=['POST']
 )
